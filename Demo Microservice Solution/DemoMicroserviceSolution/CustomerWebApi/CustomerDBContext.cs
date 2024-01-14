4426671fb25d938a0b067729b6b6eea19f0c2d8b
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2024-01-14 12:38:42 +0000

    Added CustomerDBContext class with code to create database and table

---
+++ Demo Microservice Solution/DemoMicroserviceSolution/CustomerWebApi/CustomerDBContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace CustomerWebApi
+{
+    public class CustomerDBContext : DbContext
+    {
+
+
+        public CustomerDBContext(DbContextOptions<CustomerDBContext> dbContextOptions ): base(dbContextOptions)
+        {
+            try
+            {
+
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if( databaseCreator!=null)
+                {
+                    if (!databaseCreator.CanConnect()) databaseCreator.Create();
+
+                    if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+
+                }
+
+            }
+            catch(Exception ex ) 
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+
+
+        }
+
+
+
+    }
+}
