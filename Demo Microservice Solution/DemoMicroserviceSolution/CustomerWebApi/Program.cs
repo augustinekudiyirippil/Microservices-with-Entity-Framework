ac2544d3e78c301cb81ef67d4c3b830c06d26192
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2024-01-14 12:25:52 +0000

    Added dependency injection in Program.cs to connect SQL

--- Demo Microservice Solution/DemoMicroserviceSolution/CustomerWebApi/Program.cs
+++ Demo Microservice Solution/DemoMicroserviceSolution/CustomerWebApi/Program.cs
@@ -1,8 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Database context dependecny injection
+
+var dbHost = "DEMOLAPTOP";
+var dbName = "DBcustomer";
+var dbPassword = "Hanrahan1";
+var connectionString = $"Data Source={dbHost}; Initial Catalog={dbName}; User ID=sa; Password={dbPassword}";
+
+
+builder.Services.AddDbContext<CustomerDbContext>(opt => opt.UseSqlServer(connectionString));
+
+
+//Database context dependecny injection above
+
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
